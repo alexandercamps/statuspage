aa3b900ab9bbbbc1c3c0a54bcd906ce30e09f81c
Alexander Campos <alexandercamps@gmail.com>
2021-08-17 15:16:51 -0300

    Controling execution flow in linux daemon service

--- health-check.sh
+++ health-check.sh
@@ -22,26 +22,34 @@ echo "Starting health checks with ${#KEYSARRAY[@]} configs:"
 
 mkdir -p logs
 
-for (( index=0; index < ${#KEYSARRAY[@]}; index++))
+#RUNNING_FILE="/var/www/html/statuspage-running"
+RUNNING_FILE="/f/Projetos/Frenet/src/statuspage/statuspage-running"
+while [ -f $RUNNING_FILE ];
 do
-  key="${KEYSARRAY[index]}"
-  url="${URLSARRAY[index]}"
-  echo "  $key=$url"
-
-  for i in 1 2 3 4; 
+  for (( index=0; index < ${#KEYSARRAY[@]}; index++))
   do
-    response=$(curl --write-out '%{http_code}' --silent --output /dev/null $url)
-    if [ "$response" -eq 200 ] || [ "$response" -eq 202 ] || [ "$response" -eq 301 ] || [ "$response" -eq 307 ]; then
-      result="success"
-    else
-      result="failed"
-    fi
-    if [ "$result" = "success" ]; then
-      break
-    fi
-    sleep 5
+    key="${KEYSARRAY[index]}"
+    url="${URLSARRAY[index]}"
+    echo "  $key=$url"
+
+    for i in 1 2 3 4; 
+    do
+      response=$(curl --write-out '%{http_code}' --silent --output /dev/null $url)
+      if [ "$response" -eq 200 ] || [ "$response" -eq 202 ] || [ "$response" -eq 301 ] || [ "$response" -eq 307 ]; then
+        result="success"
+      else
+        result="failed"
+      fi
+      if [ "$result" = "success" ]; then
+        break
+      fi
+      sleep 5
+    done # for retries 4 time
+    dateTime=$(date +'%Y-%m-%d %H:%M')
+    echo $dateTime, $result >> "logs/${key}_report.log"
+    echo "    $dateTime, $result"  
   done
-  dateTime=$(date +'%Y-%m-%d %H:%M')
-  echo $dateTime, $result >> "logs/${key}_report.log"
-  echo "    $dateTime, $result"  
-done
+
+  echo "Waiting for 60 seconds until next health check"
+  sleep 60 # waiting for 60 seconds, after try again
+done # while true end
